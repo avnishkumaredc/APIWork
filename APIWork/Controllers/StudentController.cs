543d91d08efe805dee64083174b74dd50d0a982b
avnishkumaredc <avnish.avnish@ecolab.com>
2023-12-07 12:52:30 +0530

    Add project files.

---
+++ APIWork/Controllers/StudentController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Net;
+
+namespace APIWork.Controllers
+{
+    [ApiController]
+    [Route("api/Student")]
+    public class StudentController : ControllerBase
+    {
+        static List<StudentModel> students = new List<StudentModel>();
+        [HttpGet]
+        public ActionResult<IEnumerable<StudentModel>> GetAllStudents()
+        {
+            return Ok(students);
+        }
+
+
+
+        [HttpGet("{id:int}",Name = "GetStudent")]        
+        public ActionResult<StudentModel> GetStudent(int id)
+        {
+            return Ok(students.Find(s => s.Id == id));
+        }
+
+
+
+        
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost]
+        public ActionResult<StudentModel> CreateNewStudent([FromBody] StudentModel newStudent)
+        {
+            var ifStudent = students.Find(s => s.Id == newStudent.Id);
+            if(ifStudent != null)
+            {
+                return BadRequest("Student with this ID already exists, Try with different ID");
+            }
+            students.Add(newStudent);
+            //return Ok(newStudent);
+            return CreatedAtRoute("GetStudent", new { id = newStudent.Id}, newStudent);
+        }
+
+
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPut]
+        public ActionResult<StudentModel> UpdateStudent([FromBody] StudentModel oldStudent)
+        {
+            var ifStudent = students.Find(s => s.Id == oldStudent.Id);
+            if(ifStudent == null)
+            {
+                return BadRequest("Student with this ID doesnot exist, Try again with different ID");
+            }
+            int index = students.IndexOf(ifStudent);
+            students[index] = oldStudent;
+            return CreatedAtRoute("GetStudent", new { id = oldStudent.Id }, oldStudent);
+
+        }
+
+
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            var ifStudent = students.Find(s => s.Id == id);
+            if (ifStudent == null)
+            {
+                return BadRequest("Student with this ID doesnot exist");
+            }
+            students.Remove(ifStudent);
+            return NoContent();
+        }
+    }
+}
---
+++ APIWork/StudentModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIWork
+{
+    public class StudentModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public char Gender { get; set; }
+        public double Fees { get; set; }
+    }
+}
